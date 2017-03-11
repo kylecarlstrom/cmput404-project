69d6cb66ab0d4fdfde6f5c2055fa62efa5e13ed9
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-11 16:44:01 -0700

    Added redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+/* https://github.com/zalmoxisus/redux-devtools-extension Mihail Diordiev (https://github.com/zalmoxisus) (MIT) */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(
-  <Provider store={createStore(reducers,
-    /* https://github.com/zalmoxisus/redux-devtools-extension Mihail Diordiev (https://github.com/zalmoxisus) (MIT) */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
     <App />
   </Provider>, document.getElementById('root')
 );
