81433ae1850ca37f66b844452ca634e1ba106f59
joshdeng <j.deng7@hotmail.com>
2017-03-11 17:50:04 -0700

    put

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ import * as types from '../types';
 import uuid from 'uuid/v1';
 
 export function addComment(comment, postId, user) {
+ //call api
+ 
+
   return {
     type: types.ADD_COMMENT,
     postId,
@@ -11,18 +14,52 @@ export function addComment(comment, postId, user) {
       author: user
     }
   };
+
 }
 
 export function addPost(post, user) {
-  return {
-    type: types.ADD_POST,
-    post: {
+  // return {
+  //   type: types.ADD_POST,
+  //   post: {
+  //     id: uuid(),
+  //     contentType: post.contentType,
+  //     title: post.title,
+  //     user_with_permission: post.user_with_permission,
+  //     author: user,
+  //     comments: []
+  //   }
+  // };
+      return function(dispatch) {
+    dispatch({type:types.ADD_POST,post: {
       id: uuid(),
       contentType: post.contentType,
       title: post.title,
       user_with_permission: post.user_with_permission,
       author: user,
       comments: []
-    }
+    }})
+    fetch('http://'+ window.location.hostname + ':8000/posts/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        title: "abc",
+        content: "def",
+        description:"ggg",
+        contentType:"ddfs",
+        author:"sdfsd",
+        visibility:"PUBLIC"
+      }),
+    })
+    .then((res) => {
+       console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
   };
-}
+  }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-
+import createLogger from 'redux-logger';
 import App from './components/App';
 import reducers from './reducers';
+
+const logger = createLogger();
 
 /* https://github.com/zalmoxisus/redux-devtools-extension Mihail Diordiev (https://github.com/zalmoxisus) (MIT) */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(
-  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)))}>
     <App />
   </Provider>, document.getElementById('root')
 );
