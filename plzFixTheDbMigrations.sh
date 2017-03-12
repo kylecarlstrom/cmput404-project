01fa2e57c674a7f55d439edeb009349af9599e00
Conner D <connerdunn7399@gmail.com>
2017-03-12 16:16:02 -0600

    fix the db for the script kiddies :lollipop:

---
+++ plzFixTheDbMigrations.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+pip install -r requirements.txt
+rm db.sqlite3
+rm server/quickstart/migrations/*
+python manage.py makemigrations
+python manage.py makemigrations quickstart
+python manage.py migrate
+python manage.py createsuperuser
