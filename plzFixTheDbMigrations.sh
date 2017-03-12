160927834a6b5a77d8e02ca2eeb0ef614fbc84e6
Conner D <connerdunn7399@gmail.com>
2017-03-12 16:22:20 -0600

    info what each line doing : plzFixTheDbMigrations

--- plzFixTheDbMigrations.sh
+++ plzFixTheDbMigrations.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+echo "Running pip install"
 pip install -r requirements.txt
+
+echo "Removing the database"
 rm db.sqlite3
+
+echo "Removing old migration files"
 rm server/quickstart/migrations/*
+
+echo "Creating the migration files"
 python manage.py makemigrations
 python manage.py makemigrations quickstart
+
+echo "Migrating the database"
 python manage.py migrate
+
+echo "Creating the superuser..."
 python manage.py createsuperuser
