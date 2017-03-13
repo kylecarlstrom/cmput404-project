a73b589d38f9089174955f7a23d0c4aa2e6f8f5f
Conner D <connerdunn7399@gmail.com>
2017-03-12 18:38:33 -0600

    Fixing whitespace + imports

--- server/quickstart/tests.py
+++ server/quickstart/tests.py
@@ -1,23 +1,21 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test import Client
 from rest_framework import status
 from rest_framework.test import APITestCase
 from models import Post, Comment, FollowingRelationship
-from django.test import Client
-from django.contrib.auth.models import User
-from .models import FollowingRelationship, Post, Comment
-
 
 class AccountTests(APITestCase):
-
     def setUp(self):
-        #from http://www.django-rest-framework.org/api-guide/testing/ 
+        # accessed on March 12, 2017
+        # from http://www.django-rest-framework.org/api-guide/testing/
         self.authorUser = User.objects.create_user('nixy', 'nixy@nixy.com','tester123')
         self.authorUser.set_password('tester123')
         self.authorUser.save()
 
         self.client.login(username='nixy', password='tester123')
         self.client.force_authenticate(user=self.authorUser)
-    
+
     def test_getPost(self):
         doesNotExistYet = "10"
         getUrl = '/posts/' + doesNotExistYet + '/'
