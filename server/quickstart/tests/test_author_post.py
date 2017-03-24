c6f6edf5111ce3cb5e605743cf882b23fdca3c38
Conner D <connerdunn7399@gmail.com>
2017-03-24 11:14:36 -0600

    populated different authors & relations

--- server/quickstart/tests/test_author_post.py
+++ server/quickstart/tests/test_author_post.py
@@ -1,7 +1,7 @@
 import base64
 from django.urls import reverse
 from django.contrib.auth.models import User
-from server.quickstart.models import Post, Author
+from server.quickstart.models import Post, Author, FollowingRelationship
 from rest_framework import status
 from rest_framework.test import APITestCase
 from requests.auth import HTTPBasicAuth
@@ -9,35 +9,52 @@ from requests.auth import HTTPBasicAuth
 class AuthorPostTest(APITestCase):
     """ This is the home of all of our tests relating to the author/post url """
 
-    AUTHOR_USER_NAME = 'aName'
+    AUTHOR_USER_NAME = 'author'
     AUTHOR_USER_PASS = 'password127'
-    AUTHOR_USER_MAIL = 'aName@example.com'
-    STRANGER_USER_NAME = 'sName'
-    STRANGER_USER_PASS = 'password127'
-    STRANGER_USER_MAIL = 'sName@example.com'
-    NOT_ACTIVE_USER_NAME = 'notActiveName'
-    NOT_ACTIVE_USER_MAIL = 'notActiveName@example.com'
-    NOT_ACTIVE_USER_PASS = 'password127'
+    AUTHOR_USER_MAIL = 'author@example.com'
 
-    def createAuthor(self, us, em, pw):
+    FRIEND_USER_NAME = 'friend'
+    FRIEND_USER_PASS = 'password127'
+    FRIEND_USER_MAIL = 'friend@example.com'
+
+    FOAF_USER_NAME = 'foaf'
+    FOAF_USER_PASS = 'password127'
+    FOAF_USER_MAIL = 'foaf@example.com'
+
+    STRANGER_USER_NAME = 'stranger'
+    STRANGER_USER_PASS = 'password127'
+    STRANGER_USER_MAIL = 'stranger@example.com'
+
+    NOT_ACTIVE_USER_NAME = 'notative'
+    NOT_ACTIVE_USER_PASS = 'password127'
+    NOT_ACTIVE_USER_MAIL = 'notative@example.com'
+
+    def createAuthor(self, us, em, pw, isActive=True):
+        authorUser = User.objects.create_user(us, em, pw)
+        authorUser.is_active = isActive
+        authorUser.save()
+        author = Author.objects.create(displayName=us, user=authorUser)
+        author.save()
+        return author
+
+    def createAuthorFriend(self, us, em, pw, friend):
         authorUser = User.objects.create_user(us, em, pw)
         authorUser.save()
         author = Author.objects.create(displayName=us, user=authorUser)
         author.save()
+        FollowingRelationship.objects.create(user=author, follows=friend)
+        FollowingRelationship.objects.create(user=friend, follows=author)
+        return author
+
 
     def setUp(self):
         """ Set up is run before each test """
-        self.createAuthor(self.AUTHOR_USER_NAME, self.AUTHOR_USER_MAIL, self.AUTHOR_USER_PASS)
+        self.createAuthor(self.NOT_ACTIVE_USER_NAME, self.NOT_ACTIVE_USER_MAIL, self.NOT_ACTIVE_USER_PASS, isActive=False)
         self.createAuthor(self.STRANGER_USER_NAME, self.STRANGER_USER_MAIL, self.STRANGER_USER_PASS)
+        a = self.createAuthor(self.AUTHOR_USER_NAME, self.AUTHOR_USER_MAIL, self.AUTHOR_USER_PASS)
+        b = self.createAuthorFriend(self.FRIEND_USER_NAME, self.FRIEND_USER_MAIL, self.FRIEND_USER_PASS, a)
+        self.createAuthorFriend(self.FOAF_USER_NAME, self.FOAF_USER_MAIL, self.FOAF_USER_PASS, b)
 
-        self.unAuthorizedUser = User.objects.create_user(self.NOT_ACTIVE_USER_NAME,
-                                                         self.NOT_ACTIVE_USER_MAIL,
-                                                         self.NOT_ACTIVE_USER_PASS)
-        self.unAuthorizedUser.is_active = False  # this is crucial to make an unAuthorizedUser
-        self.unAuthorizedUser.save()
-        unAuthAuthor = Author.objects.create(displayName=self.NOT_ACTIVE_USER_NAME,
-                                             user=self.unAuthorizedUser)
-        unAuthAuthor.save()
 
     def getBasicAuthHeader(self, us, pw):
         """ Returns the b64encoded string created for a user and password to be used in the header """
